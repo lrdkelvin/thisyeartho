6d670a9968eca58896954a6f5d5c3a218389f12d
Miguel Celis <60907205+mcelis025@users.noreply.github.com>
2020-06-13 01:00:48 -0600

    added container class (index.html) // added js button functions to extract url

--- public/js/factcheck.js
+++ public/js/factcheck.js
@@ -1,3 +1,10 @@
 // This is our basic javascript file that will include the logic for buttons
-//and probably calls to outside APIs 
-//I renamed the file factcheck because there was already an index.js in the models folder
+// and probably calls to outside APIs
+// I renamed the file factcheck because there was already an index.js in the models folder
+
+// button for article-parser
+$("#extractInfo").on("click", function() {
+  urlToBeValidated = $("#itemURL").val();
+
+  console.log(urlToBeValidated);
+});
