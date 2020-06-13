8b875ae2b97d92eb93a67ccf0f1d04c9d3617507
Miguel Celis <60907205+mcelis025@users.noreply.github.com>
2020-06-13 13:32:25 -0600

    removed port // still working on submit button for article parse

--- public/js/factcheck.js
+++ public/js/factcheck.js
@@ -2,23 +2,20 @@
 // and probably calls to outside APIs
 // I renamed the file factcheck because there was already an index.js in the models folder
 $(function() {
+  // button for article-parser
+  $("#extractInfo").on("click", function() {
+    event.preventDefault();
 
-// button for article-parser
-$("#extractInfo").on("click", function() {
-  event.preventDefault();
-
-  var newURL = {
-    url: $("#itemURL").val(),
-  };
-
+    var newURL = {
+      url: $("#itemURL").val(),
+    };
+    console.log(newURL);
     // Send the POST request.
     $.ajax("/api/articleVal", {
       type: "POST",
-      data: newURL
-    }).then(
-      function() {
-        location.reload();
-      }
-    );
+      data: newURL,
+    }).then(function() {
+      location.reload();
+    });
   });
 });
