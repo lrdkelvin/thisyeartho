3fe9c36ca761f6035797cb5886d36b75bfe9bb0a
Miguel Celis <60907205+mcelis025@users.noreply.github.com>
2020-06-15 17:58:59 -0600

    replaced var names

--- public/js/add.js
+++ public/js/add.js
@@ -1,66 +1,64 @@
 $(document).ready(function() {
   // Gets an optional query string from our url (i.e. ?post_id=23)
   var url = window.location.search;
-  var postId;
+  var itemId;
   // Sets a flag for whether or not we're updating a post to be false initially
   var updating = false;
 
   // If we have this section in our url, we pull out the post id from the url
   // In localhost:8080/cms?post_id=1, postId is 1
   if (url.indexOf("?post_id=") !== -1) {
-    postId = url.split("=")[1];
-    getPostData(postId);
+    itemId = url.split("=")[1];
+    getItemData(itemId);
   }
 
   // Getting jQuery references to the post body, title, form, and category select
-  var bodyInput = $("#body");
-  var titleInput = $("#title");
-  var cmsForm = $("#cms");
-  var postCategorySelect = $("#category");
+  var urlInput = $("#urlToBe");
+  var addForm = $("#addNew");
+  var categorySelect = $("#category");
   // Giving the postCategorySelect a default value
-  postCategorySelect.val("Personal");
+  categorySelect.val("Article");
   // Adding an event listener for when the form is submitted
-  $(cmsForm).on("submit", function handleFormSubmit(event) {
+  $(addForm).on("submit", function handleFormSubmit(event) {
     event.preventDefault();
     // Wont submit the post if we are missing a body or a title
-    if (!titleInput.val().trim() || !bodyInput.val().trim()) {
+    if (!urlInput.val().trim()) {
       return;
     }
     // Constructing a newPost object to hand to the database
-    var newPost = {
-      title: titleInput.val().trim(),
-      body: bodyInput.val().trim(),
-      category: postCategorySelect.val()
+    var newItem = {
+      url: urlInput.val().trim(),
+      rating: null,
+      category: categorySelect.val()
     };
 
-    console.log(newPost);
+    console.log(newItem);
 
     // If we're updating a post run updatePost to update a post
     // Otherwise run submitPost to create a whole new post
     if (updating) {
-      newPost.id = postId;
-      updatePost(newPost);
+      newItem.id = itemId;
+      updateItem(newItem);
     }
     else {
-      submitPost(newPost);
+      submitItem(newItem);
     }
   });
 
   // Submits a new post and brings user to blog page upon completion
-  function submitPost(Post) {
-    $.post("/api/posts/", Post, function() {
-      window.location.href = "/blog";
+  function submitItem(item) {
+    $.post("/api/articles/", item, function() {
+      window.location.href = "/admin";
     });
   }
 
   // Gets post data for a post if we're editing
-  function getPostData(id) {
-    $.get("/api/posts/" + id, function(data) {
+  function getItemData(id) {
+    $.get("/api/articles/" + id, function(data) {
       if (data) {
         // If this post exists, prefill our cms forms with its data
-        titleInput.val(data.title);
-        bodyInput.val(data.body);
-        postCategorySelect.val(data.category);
+        urlInput.val(data.title);
+        categorySelect.val(data.category);
         // If we have a post with this id, set a flag for us to know to update the post
         // when we hit submit
         updating = true;
@@ -69,14 +67,14 @@ $(document).ready(function() {
   }
 
   // Update a given post, bring user to the blog page when done
-  function updatePost(post) {
+  function updateItem(item) {
     $.ajax({
       method: "PUT",
-      url: "/api/posts",
-      data: post
+      url: "/api/articles",
+      data: item
     })
       .then(function() {
-        window.location.href = "/blog";
+        window.location.href = "/admin";
       });
   }
 });
