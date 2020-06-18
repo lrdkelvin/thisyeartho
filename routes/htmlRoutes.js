9b3ef2d692ac5d3e7559c5b6bf281c5a53d28f57
Miguel Celis <60907205+mcelis025@users.noreply.github.com>
2020-06-17 19:26:41 -0600

    fixing branch

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -80,11 +80,7 @@ module.exports = function(app, passport) {
   });
 
   //once logged in as normal user -- add.html
-<<<<<<< HEAD
   app.get("/dashboard", function(req, res) {
-=======
-  app.get("/add", function(req, res) {
->>>>>>> a2b27825e6137212a4b27fbb5c556cd3273fde6c
     res.sendFile(path.join(__dirname, "../public/add.html"));
   });
 };
